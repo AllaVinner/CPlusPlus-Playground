078f1c7a3167cfaf30ea4c26fd16bcb9b929b916
AllaVinner <joel.w.ottosson@gmail.com>
2022-10-27 20:59:21 +0200

    Added double pointer in pointer example.

--- coursera/w2/pointers.c
+++ coursera/w2/pointers.c
@@ -9,6 +9,18 @@ int main(void)
     printf("a: %d\n", a);
     *p = 24;
     printf("a: %d\n", a);
+    printf("-----------------\n");
+    printf("p: %d\n", p);
+    printf("*p: %d\n", *p);
+    printf("&p: %d\n", &p);
+    printf("-----------------\n");
+    int a2 = 100;
+    int* p2 = &a2;
+    int** pp2 = &p2;
+    printf("\t & \t . \t *\n");
+    printf("a2: %d \t%d \t%d \n", &a2, a2, 0);
+    printf("p2: %d \t%d \t%d \n", &p2, p2, *p2);
+    printf("pp2: %d \t%d \t%d \n", &pp2, pp2, *pp2);
 }
 
 
