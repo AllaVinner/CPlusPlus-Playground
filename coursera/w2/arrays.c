d41155351861f0161c225d41ba860d1d8728191e
AllaVinner <joel.w.ottosson@gmail.com>
2022-10-27 18:58:50 +0200

    Added string example to array script.

--- coursera/w2/arrays.c
+++ coursera/w2/arrays.c
@@ -3,12 +3,20 @@
 int main(void)
 {
     // Len must be known at compile time
-    int data [10];
+    int data [10] = {0};
     for(int i=0; i<10; ++i)
     {
         printf("Item %d Val %d\n", i, data[i]);
     }
 
+     // "ab" is represented as ['a', 'b', '\0']
+     // where '\0' is the sentinel value. Marks th
+    printf("\n");
+    char string [5] = {'a', 'b', 'c', 'd', 'e'};
+    for(int i=0; i < 5; i++)
+    {
+        printf("%c*", string[i]);
+    }
 }
 
 
